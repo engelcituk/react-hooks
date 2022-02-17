f4b790eff5975b4014f2afeb8c108e4deb1a7238
cituk <programaciondyn3.mx@sandos.com>
2022-02-17 01:07:56 -0500

    useMemo: evita cálculos innecesarios en componentes

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,7 +1,25 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useReducer, useMemo } from 'react'
+
+const initialState = {
+    favorites: []
+}
+
+const favoriteReducer = (state, action ) => {
+    switch (action.type){
+        case 'ADD_TO_FAVORITE':
+        return {
+            ...state,
+            favorites:[...state.favorites, action.payload ]
+        }
+        default:
+            return state
+    }
+}
 
 const Characters = () => {
     const [characters, setCharacters ] = useState([])
+    const [ favorites, dispatch ] = useReducer(favoriteReducer, initialState)
+    const [ search , setSearch ] = useState('')
 
     //usa dos parametros, una funcion anonima donde está la logica
     //el segundo la variable que va estar escuchando en dado caso que tenga un cambio
@@ -11,11 +29,32 @@ const Characters = () => {
         .then( data => setCharacters( data.results ) )
     }, []) //cuando no tiene nada que escuchar se le pasa como 2do parametro un arreglo vacio []
 
+    const handleClick = favorite => {
+        dispatch( { type: 'ADD_TO_FAVORITE', payload: favorite } )
+    }
+
+    const handleSearch= (event) => {
+        console.log(event.target.value )
+        setSearch( event.target.value )
+    }
+
+    /*const filteredCharacters = characters.filter( (user) => {
+        return user.name.toLowerCase().includes( search.toLowerCase() )
+    })*/
+
+    const filteredCharacters = useMemo( () =>  //usa una funcion anonima
+        characters.filter( (user) => {
+            return user.name.toLowerCase().includes( search.toLowerCase() )
+        }), [characters, search] // segundo parametros, escuchando sobre los personajes y search
+    )
+
     return (
         <div className="row">
+            <h2>Favoritos</h2>
+            
             {
-                characters.map( (character, index)  => (
-                    <div className='col-md-3 mb-2' key={index}>
+                favorites.favorites.map( (character ) => (
+                    <div className='col-md-2 mb-2' key={character.id}>
                         <div className="card" style={{width: '18rem'}}>
                             <img src={character.image} className="card-img-top" alt={character.name }/>
                             <div className="card-body">
@@ -27,7 +66,26 @@ const Characters = () => {
                     </div>
                 ))
             }
-            
+            <div className="input-group mb-3">
+                <input type="text" className="form-control" value={search} onChange={handleSearch}/>
+                {/* <button className="btn btn-primary" type="button" id="button-addon2" onClick={handleSearch}>Buscar</button> */}
+            </div>
+            <h2>Personajes api</h2>
+            {
+                filteredCharacters.map( (character)  => (
+                    <div className='col-md-3 mb-2' key={character.id}>
+                        <div className="card" style={{width: '18rem'}}>
+                            <img src={character.image} className="card-img-top" alt={character.name }/>
+                            <div className="card-body">
+                                <h5 className="card-title">{character.name }</h5>
+                                <p className="card-text">{character.species}</p>
+                                <p className="card-text">{character.origin.name}</p>
+                                <button className="btn btn-primary" type='button' onClick={ () => handleClick(character) }>Add to favorites</button>
+                            </div>
+                        </div>
+                    </div>
+                ))
+            }
         </div>
     )
 }
